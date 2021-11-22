1ed014e3e453a0a1ed6c57b923e4c6262dea958c
dss-vaio <denis2maksimov@gmail.com>
2021-11-22 09:54:53 +0300

    Initial commit

---
+++ src/main/java/ParserSitePrice.java
@@ -0,0 +1,46 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.net.URL;
+
+public class ParserSitePrice {
+    public static String pageParseDiapus() throws IOException {
+        String url = "https://diapuls.ru/product/test-poloski-ime-dc-50/";
+        Document page = Jsoup.parse(new URL(url), 3000);
+        Element price = page.select("div.price").first();
+        return price.text();
+    }
+
+    public static String pageParseTestpoloska() throws IOException {
+        String url = "https://www.test-poloska.ru/catalog/teststripes/imedc50.html";
+        Document page = Jsoup.parse(new URL(url), 3000);
+        Element price = page.select("tbody").get(10);
+        Elements priceTS = price.getElementsByAttribute("nowrap");
+        return priceTS.text();
+    }
+
+    public static String pageParseMedMag() throws IOException {
+        String url = "https://www.medmag.ru/index.php?productID=792&from=ya";
+        Document page = Jsoup.parse(new URL(url), 3000);
+        Element price = page.select("font.price").first();
+        return price.text();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String pageDiapuls = pageParseDiapus();
+        String pageMedMag = pageParseMedMag();
+        String pageTP = pageParseTestpoloska();
+       System.out.println("Цена полосок ime-dc на сайте diapuls.ru: "
+               + pageDiapuls
+               + System.lineSeparator()
+               + "Цена полосок ime-dc на сайте medmag.ru:  "
+               + pageMedMag
+               + System.lineSeparator()
+               + "Цена полосок ime-dc на сайте test-poloska.ru:  "
+               + pageTP);
+    }
+}
+
