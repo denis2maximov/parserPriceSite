df62ddfc88ca216b0ab50cb044fab01415ee6ea3
mdv <denis2maksimov@gmail.com>
2021-12-17 23:17:28 +0300

    добавил вывод результата в окне (JavaFx)

--- src/main/java/ParserSitePrice.java
+++ src/main/java/ParserSitePrice.java
@@ -1,51 +1,74 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class ParserSitePrice {
+import static org.jsoup.Jsoup.parse;
+
+public class ParserSitePrice extends Application {
     public static String pageParseDiapus() throws IOException {
         String url = "https://diapuls.ru/product/test-poloski-ime-dc-50/";
-        Document page = Jsoup.parse(new URL(url), 3000);
+        Document page = parse(new URL(url), 3000);
         Element price = page.select("div.price").first();
         return price.text();
     }
 
     public static String pageParseTestpoloska() throws IOException {
         String url = "https://www.test-poloska.ru/catalog/teststripes/imedc50.html";
-        Document page = Jsoup.parse(new URL(url), 3000);
+        Document page = parse(new URL(url), 3000);
         Element price = page.select("tbody").get(10);
         Elements priceTS = price.getElementsByAttribute("nowrap");
-        List<String> list = new ArrayList<>();
-        Collections.addAll(list, priceTS.text().split(" "));
-        return list.get(1);
-    }
+        return Arrays.stream(priceTS.text()
+                .split(" "))
+                .skip(1)
+                .reduce((x, y) -> x + " " + y)
+                .stream()
+                .findFirst()
+                .get();
+        }
 
     public static String pageParseMedMag() throws IOException {
         String url = "https://www.medmag.ru/index.php?productID=792&from=ya";
-        Document page = Jsoup.parse(new URL(url), 3000);
+        Document page = parse(new URL(url), 3000);
         Element price = page.select("font.price").first();
         return price.text();
     }
 
-    public static void main(String[] args) throws IOException {
-        String pageDiapuls = pageParseDiapus();
-        String pageMedMag = pageParseMedMag();
-        String pageTP = pageParseTestpoloska();
-       System.out.println("Цена полосок ime-dc на сайте diapuls.ru: "
-               + pageDiapuls
-               + System.lineSeparator()
-               + "Цена полосок ime-dc на сайте medmag.ru:  "
-               + pageMedMag
-               + System.lineSeparator()
-               + "Цена полосок ime-dc на сайте test-poloska.ru:  "
-               + pageTP);
+    public static void main(String[] args) {
+        Application.launch();
+   }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setWidth(600);
+        stage.setHeight(500);
+        stage.setTitle("Поехали парсить сайты!!");
+        Label diapuls = new Label("Цена полосок ime-dc на сайте diapuls.ru: "
+                + pageParseDiapus()
+                + System.lineSeparator()
+                + "Цена полосок ime-dc на сайте medmag.ru:  "
+                + pageParseMedMag()
+                + System.lineSeparator()
+                + "Цена полосок ime-dc на сайте test-poloska.ru: "
+                + pageParseTestpoloska());
+
+        diapuls.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(diapuls);
+        stage.setScene(scene);
+        stage.show();
     }
 }
 
